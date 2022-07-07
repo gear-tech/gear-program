b0381b238dc4a1ecd8720326d3935e197e516624
clearloop <26088946+clearloop@users.noreply.github.com>
2022-07-07 17:49:14 +0800

    feat(cmd): introduce send_message and send_reply

--- src/cmd/reply.rs
+++ src/cmd/reply.rs
@@ -1,1 +1,39 @@
+//! Command `reply`
+use crate::{
+    api::{
+        generated::api::{gear::calls::SendReply, runtime_types::gear_core::ids::MessageId},
+        Api,
+    },
+    Result,
+};
+use structopt::StructOpt;
 
+#[derive(StructOpt, Debug)]
+pub struct Reply {
+    /// Reply to
+    pub reply_to_id: String,
+    /// Reply payload
+    payload: String,
+    /// Reply gas limit
+    gas_limit: u64,
+    /// Reply value
+    value: u128,
+}
+
+impl Reply {
+    pub async fn exec(&self, api: Api) -> Result<()> {
+        let mut reply_to_id = [0; 32];
+        reply_to_id
+            .copy_from_slice(&mut hex::decode(self.reply_to_id.trim_start_matches("0x"))?.as_ref());
+
+        api.send_reply(SendReply {
+            reply_to_id: MessageId(reply_to_id),
+            payload: hex::decode(self.payload.trim_start_matches("0x"))?,
+            gas_limit: self.gas_limit,
+            value: self.value,
+        })
+        .await?;
+
+        Ok(())
+    }
+}
--- src/cmd/send.rs
+++ src/cmd/send.rs
@@ -1,1 +1,39 @@
+//! Command `send`
+use crate::{
+    api::{
+        generated::api::{gear::calls::SendMessage, runtime_types::gear_core::ids::ProgramId},
+        Api,
+    },
+    Result,
+};
+use structopt::StructOpt;
 
+#[derive(StructOpt, Debug)]
+pub struct Send {
+    /// Send to
+    pub destination: String,
+    /// Send payload
+    pub payload: String,
+    /// Send gas limit
+    pub gas_limit: u64,
+    /// Send value
+    pub value: u128,
+}
+
+impl Send {
+    pub async fn exec(&self, api: Api) -> Result<()> {
+        let mut destination = [0; 32];
+        destination
+            .copy_from_slice(&mut hex::decode(self.destination.trim_start_matches("0x"))?.as_ref());
+
+        api.send_message(SendMessage {
+            destination: ProgramId(destination),
+            payload: hex::decode(self.payload.trim_start_matches("0x"))?,
+            gas_limit: self.gas_limit,
+            value: self.value,
+        })
+        .await?;
+
+        Ok(())
+    }
+}
