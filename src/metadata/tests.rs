4528c19a097abcf629b7883bdd377186047475d9
clearloop <26088946+clearloop@users.noreply.github.com>
2022-08-10 00:33:42 +0800

    feat(metadata): test metadata formatting

--- src/metadata/tests.rs
+++ src/metadata/tests.rs
@@ -6,12 +6,36 @@ use scale_info::{
     form::Form, IntoPortable, MetaType, PortableRegistry, Registry, TypeDef, TypeInfo,
 };
 
-#[derive(TypeInfo)]
-pub struct Composite {
-    string: String,
-    number: u32,
-    array: [u8; 32],
+const DEMO_METADATA: &str = r#"
+Metadata {
+    meta_init_input: MessageInitIn {
+        amount: u8,
+        currency: String,
+    },
+    meta_init_output: MessageInitOut {
+        exchange_rate: Result<u8, u8>,
+        sum: u8,
+    },
+    meta_async_init_input: MessageInitAsyncIn {
+        empty: (),
+    },
+    meta_async_init_output: MessageInitAsyncOut {
+        empty: (),
+    },
+    meta_handle_input: MessageIn {
+        id: Id,
+    },
+    meta_handle_output: MessageOut {
+        res: Option<Wallet>,
+    },
+    meta_async_handle_input: MessageHandleAsyncIn {
+        empty: (),
+    },
+    meta_async_handle_output: MessageHandleAsyncOut {
+        empty: (),
+    },
 }
+"#;
 
 #[test]
 fn test_parsing_metadata() {
@@ -36,13 +60,8 @@ fn test_parsing_metadata() {
         }
     );
 
-    println!(
-        "{:#}",
-        metadata,
-        // format!("{:#}", metadata)
-        //     .replace('"', "")
-        //     .replace("\\n", "\n")
-        // .replace('"', "")
-        // .replace("\n", &format!("\n{:>4}", ' '))
+    assert_eq!(
+        DEMO_METADATA.trim(),
+        &format!("{:#}", metadata).replace('"', "")
     );
 }
