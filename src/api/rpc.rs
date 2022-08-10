f10102197766bc4c4af6b48648f9bf47d565b496
clearloop <26088946+clearloop@users.noreply.github.com>
2022-08-11 07:46:47 +0800

    feat(api): introduce storage APIs for programs

--- src/api/rpc.rs
+++ src/api/rpc.rs
@@ -1,14 +1,20 @@
 //! gear api rpc methods
 use crate::{
-    api::{types, Api},
-    Result,
+    api::{generated::api::runtime_types::gear_common::ActiveProgram, types, Api},
+    Error, Result,
 };
-use std::sync::Arc;
+use hex::ToHex;
+use parity_scale_codec::Decode;
+use std::{collections::HashMap, sync::Arc};
 use subxt::{
     rpc::{rpc_params, ClientT},
-    sp_core::{Bytes, H256},
+    sp_core::{storage::StorageKey, Bytes, H256},
     RpcClient,
 };
+
+const GPROG: [u8; 9] = *b"g::prog::";
+const GPAGES: [u8; 10] = *b"g::pages::";
+const SEPARATOR: [u8; 2] = *b"::";
 
 impl Api {
     /// get rpc client
@@ -21,8 +27,6 @@ impl Api {
         AsRef::<[u8; 32]>::as_ref(self.signer.account_id()).into()
     }
 
-    /// rpc call
-    ///
     /// gear_getInitGasSpent
     pub async fn get_init_gas_spent(
         &self,
@@ -40,10 +44,8 @@ impl Api {
             .map_err(Into::into)
     }
 
-    #[allow(dead_code)]
-    /// rpc call
-    ///
     /// gear_getHandleGasSpent
+    #[allow(dead_code)]
     pub async fn get_handle_gas_spent(
         &self,
         dest: H256,
@@ -58,5 +60,44 @@ impl Api {
             )
             .await
             .map_err(Into::into)
+    }
+
+    /// Get active program from program id.
+    pub async fn gprog(&self, pid: H256) -> Result<ActiveProgram> {
+        let bytes = self
+            .runtime
+            .client
+            .storage()
+            .fetch_raw(StorageKey([GPROG.as_slice(), &pid.0].concat()), None)
+            .await?
+            .ok_or(Error::ProgramNotFound(pid.encode_hex()))?;
+
+        Ok(ActiveProgram::decode(&mut bytes.0.as_ref())?)
+    }
+
+    /// Get pages of active program.
+    pub async fn gpages(&self, pid: H256, program: ActiveProgram) -> Result<HashMap<u32, Vec<u8>>> {
+        let mut pages = HashMap::new();
+        let prefix = [GPAGES.as_slice(), &pid.0, &SEPARATOR].concat();
+        for page in program.pages_with_data {
+            let value = self
+                .runtime
+                .client
+                .storage()
+                .fetch_raw(
+                    StorageKey([prefix.as_slice(), &page.0.to_le_bytes()].concat()),
+                    None,
+                )
+                .await?
+                .ok_or(Error::PageNotFound(page.0, pid.encode_hex()))?;
+            pages.insert(page.0, value.0);
+        }
+
+        Ok(pages)
+    }
+
+    /// Get program pages from program id.
+    pub async fn program_pages(&self, pid: H256) -> Result<HashMap<u32, Vec<u8>>> {
+        self.gpages(pid.clone(), self.gprog(pid).await?).await
     }
 }
