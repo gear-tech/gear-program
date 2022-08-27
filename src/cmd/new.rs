86f72b6b4c2ea456678fe8144890ccdd21c523f1
clearloop <26088946+clearloop@users.noreply.github.com>
2022-08-27 14:40:38 +0800

    feat(reigstry): remove apps registry

--- src/cmd/new.rs
+++ src/cmd/new.rs
@@ -1,10 +1,5 @@
 //! command new
-use crate::{registry, result::Result};
-use std::{
-    fs::{self, DirEntry},
-    io,
-    path::PathBuf,
-};
+use crate::result::Result;
 use structopt::StructOpt;
 
 /// Create a new gear program
@@ -17,62 +12,17 @@ pub struct New {
 impl New {
     /// run command new
     pub async fn exec(&self) -> Result<()> {
-        registry::init().await?;
-        let templates = templates()?;
+        // let templates = templates()?;
 
         if let Some(template) = &self.template {
-            if templates.contains(template) {
-                copy_dir_all(&registry::GEAR_APPS_PATH.join(&template), &template.into())?;
-            } else {
-                crate::template::create(template)?;
-            }
+            crate::template::create(template)?;
 
             println!("Successfully created {}!", template);
             return Ok(());
         }
 
-        println!("AVAILABLE TEMPLATES: \n\t{}", templates.join("\n\t"));
+        // println!("AVAILABLE TEMPLATES: \n\t{}", templates.join("\n\t"));
 
         Ok(())
     }
-}
-
-/// get all templates
-fn templates() -> Result<Vec<String>> {
-    Ok(fs::read_dir(&*registry::GEAR_APPS_PATH)?
-        .filter_map(|maybe_path: io::Result<DirEntry>| {
-            if let Ok(p) = maybe_path {
-                let path = p.path();
-                if !path.is_dir() {
-                    return None;
-                }
-
-                if let Some(file) = path.file_name() {
-                    let name = file.to_string_lossy();
-                    if !name.starts_with('.') {
-                        return Some(name.into());
-                    }
-                }
-
-                None
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<String>>())
-}
-
-/// copy -r
-fn copy_dir_all(src: &PathBuf, dst: &PathBuf) -> Result<()> {
-    fs::create_dir_all(&dst)?;
-    for entry in fs::read_dir(src)? {
-        let entry = entry?;
-        let path = entry.path();
-        if path.is_dir() {
-            copy_dir_all(&path, &dst.join(entry.file_name()))?;
-        } else {
-            fs::copy(entry.path(), &dst.join(entry.file_name()))?;
-        }
-    }
-    Ok(())
 }
--- src/cmd/update.rs
+++ src/cmd/update.rs
@@ -1,14 +1,11 @@
 //! command update
-use crate::{registry, result::Result};
+use crate::result::Result;
 use std::process::{self, Command};
 use structopt::StructOpt;
 
 /// Update resources
 #[derive(Debug, StructOpt)]
 pub struct Update {
-    /// Update gear examples
-    #[structopt(short, long)]
-    pub examples: bool,
     /// Update self
     #[structopt(short, long)]
     pub gear: bool,
@@ -28,22 +25,10 @@ impl Update {
         Ok(())
     }
 
-    /// update examples
-    async fn update_examples(&self) -> Result<()> {
-        registry::update().await?;
-
-        Ok(())
-    }
-
     /// exec command update
     pub async fn exec(&self) -> Result<()> {
-        registry::init().await?;
         if self.gear {
             self.update_self().await?;
-        }
-
-        if self.examples {
-            self.update_examples().await?;
         }
 
         Ok(())
